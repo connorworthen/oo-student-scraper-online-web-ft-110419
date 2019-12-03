069ada0d54ca477089117d5a974a979d13f237e2
connor worthen <connorw220@protonmail.com>
2019-12-03 02:07:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,9 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-
+    students_array.each do |student_hash|
+      Student.new(student_hash)
+    end
   end
 
   def add_student_attributes(attributes_hash)
@@ -19,6 +21,6 @@ class Student
   end
 
   def self.all
-    @@all 
+    @@all
   end
 end
